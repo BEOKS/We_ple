3bef94c973545e288e42eedd6aacf857ddaa1ade
gowls4023 <33483699+gowls4023@users.noreply.github.com>
2018-12-28 18:24:11 +0900

    create empty_activity

---
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/LoginActivity/version02/empty_activity.java
@@ -0,0 +1,13 @@
+package com.example.beoks.gameis.weple.Activity.LoginActivity.version02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class empty_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_activity2);
+    }
+}
