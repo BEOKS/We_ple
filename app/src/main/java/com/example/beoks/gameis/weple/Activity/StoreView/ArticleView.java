75711ac275c489d50e25ce3119bbe50a7fe92f67
JAESEONGLEE <lee01042000@gmail.com>
2019-01-01 18:48:31 +0900

    update TODO

--- app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/ArticleView.java
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/ArticleView.java
@@ -1,7 +1,7 @@
 package com.example.beoks.gameis.weple.Activity.StoreView;
 
 import com.example.beoks.gameis.weple.DataClass.Store.Article;
-
+//TODO for gowls4023
 public class ArticleView {
     public Article data;
     public ArticleView(Article article){
