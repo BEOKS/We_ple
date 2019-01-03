b31a33114454e518cd76a7039e9fe387c2b760c4
gowls4023 <33483699+gowls4023@users.noreply.github.com>
2019-01-03 18:37:38 +0900

    update ArticleView and MenuView

--- app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/ArticleView.java
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/ArticleView.java
@@ -1,10 +1,62 @@
 package com.example.beoks.gameis.weple.Activity.StoreView;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.example.beoks.gameis.weple.DataClass.Store.Article;
+import com.example.beoks.gameis.weple.R;
+
 //TODO for gowls4023
-public class ArticleView {
-    public Article data;
-    public ArticleView(Article article){
+public class ArticleView extends LinearLayout implements View.OnClickListener{
+    public Article article;
+    TextView article_title;
+    Button article_button_edit;
+    TextView article_content;
+
+    public ArticleView(Context context, Article article) {
+        super(context);
+        initView();
+    }
+
+    public ArticleView(Context context, Article article, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+        //getAttrs(attrs);
+    }
+
+    public ArticleView(Context context, Article article, AttributeSet attrs, int defStyle) {
+        super(context, attrs);
+        initView();
+        //getAttrs(attrs, defStyle);
+    }
+
+    private void initView() {
+        String inflaterService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(inflaterService);
+        View view = layoutInflater.inflate(R.layout.article_view, this, false);
+        addView(view);
+
+        article_title = findViewById(R.id.article_title);
+        article_button_edit = findViewById(R.id.article_button_edit);
+        article_content = findViewById(R.id.article_content);
+
+        article_button_edit.setOnClickListener(this);
+    }
+
+    public void setTitle(String txt){
+        article_title.setText(txt);
+    }
+
+    public void setContent(String txt){
+        article_title.setText(txt);
+    }
+
+    public void onClick(View v){
 
     }
 }
--- app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/MenuView.java
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/StoreView/MenuView.java
@@ -1,10 +1,62 @@
 package com.example.beoks.gameis.weple.Activity.StoreView;
 
-import com.example.beoks.gameis.weple.DataClass.Store.Menu;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MenuView  {
+import com.example.beoks.gameis.weple.DataClass.Store.Menu;
+import com.example.beoks.gameis.weple.R;
+
+public class MenuView extends LinearLayout {
     public Menu menu;
-    public MenuView(Menu menu){
+    ImageView menu_image;
+    TextView menu_name;
+    TextView menu_price;
+
+    public MenuView(Context context, Menu menu) {
+        super(context);
         this.menu =menu;
+        initView();
+    }
+
+    public MenuView(Context context, Menu menu, AttributeSet attrs) {
+        super(context, attrs);
+        this.menu =menu;
+        initView();
+        //getAttrs(attrs);
+    }
+
+    public MenuView(Context context, Menu menu, AttributeSet attrs, int defStyle) {
+        super(context, attrs);
+        this.menu =menu;
+        initView();
+        //getAttrs(attrs, defStyle);
+    }
+
+    private void initView() {
+        String inflaterService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(inflaterService);
+        View view = layoutInflater.inflate(R.layout.menu_view, this, false);
+        addView(view);
+
+        menu_image = findViewById(R.id.menu_image);
+        menu_name = findViewById(R.id.menu_name);
+        menu_price = findViewById(R.id.menu_price);
+    }
+
+    public void setImage(int id){
+        menu_image.setImageDrawable(getResources().getDrawable(id));
+    }
+
+    public void setName(String txt){
+        menu_name.setText(txt);
+    }
+
+    public void setPrice(String txt){
+        menu_price.setText(txt);
     }
 }
