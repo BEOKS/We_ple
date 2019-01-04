3f1993a012b8535d017019a8e09928c1d7d230c7
JAESEONGLEE <lee01042000@gmail.com>
2019-01-04 15:42:03 +0900

    revise error

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f09002c;
-        public static final int end = 0x7f09005f;
-        public static final int gone = 0x7f090071;
-        public static final int invisible = 0x7f090085;
-        public static final int left = 0x7f090089;
-        public static final int packed = 0x7f0900b7;
-        public static final int parent = 0x7f0900b9;
-        public static final int percent = 0x7f0900bc;
-        public static final int right = 0x7f0900c2;
-        public static final int spread = 0x7f0900e9;
-        public static final int spread_inside = 0x7f0900ea;
-        public static final int start = 0x7f0900ef;
-        public static final int top = 0x7f090110;
-        public static final int wrap = 0x7f090124;
+        public static final int end = 0x7f09005e;
+        public static final int gone = 0x7f090070;
+        public static final int invisible = 0x7f090084;
+        public static final int left = 0x7f090088;
+        public static final int packed = 0x7f0900b6;
+        public static final int parent = 0x7f0900b8;
+        public static final int percent = 0x7f0900bb;
+        public static final int right = 0x7f0900c1;
+        public static final int spread = 0x7f0900e8;
+        public static final int spread_inside = 0x7f0900e9;
+        public static final int start = 0x7f0900ee;
+        public static final int top = 0x7f09010f;
+        public static final int wrap = 0x7f090123;
     }
     public static final class styleable {
         private styleable() {}
---
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/CommonActivity/EditArticleView.java
@@ -0,0 +1,10 @@
+package com.example.beoks.gameis.weple.Activity.CommonActivity;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class EditArticleView extends LinearLayout {
+    public EditArticleView(Context context) {
+        super(context);
+    }
+}
