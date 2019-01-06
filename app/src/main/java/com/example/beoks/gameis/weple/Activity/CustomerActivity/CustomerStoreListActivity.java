5f747ad01efd2eeb31da0f0b99feabf446c99de2
gowls4023 <33483699+gowls4023@users.noreply.github.com>
2019-01-06 18:11:51 +0900

    update CustomerActivity

--- app/src/main/java/com/example/beoks/gameis/weple/Activity/CustomerActivity/CustomerStoreListActivity.java
+++ app/src/main/java/com/example/beoks/gameis/weple/Activity/CustomerActivity/CustomerStoreListActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -15,11 +16,24 @@ import java.util.HashMap;
 
 public class CustomerStoreListActivity extends AppCompatActivity {
 
+    Button menuButton;
+    EditText searchText;
+    Button searchButton;
+
+    Button horizonButton1;
+    Button horizonButton2;
+    Button horizonButton3;
+    Button horizonButton4;
+    Button horizonButton5;
+    Button horizonButton6;
+    Button horizonButton7;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_list2);
         HashMap<String,Button> hashMap;
+
     }
 
 }
